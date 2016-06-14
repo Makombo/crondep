d6c94183b9703461fd8a67c794d96e2a330fafb3
makombo <chisomo.sakala@mombo.solutions>
2016-06-14 08:11:22 -0500

    closer to production, consolidated rmstuff

--- config.sh
+++ config.sh
@@ -4,6 +4,5 @@
 my_gitdir="$HOME/ec2site.git"
 my_worktree=/opt/bitnami/apache2/htdocs/ec2site.com
 my_origin="ssh://git@bitbucket.org/makombo/test.git"
-#git clone git clone https://makombo@bitbucket.org/makombo/test.git
+#https://makombo@bitbucket.org/makombo/test.git
 
-source rmstuff.sh
--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cd $(dirname "$0")
-
 source config.sh
 
 function error_exit {
