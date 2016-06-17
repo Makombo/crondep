2675c197ffaa32fff844fd74f8034ee50b1ad035
Somo Makombo <chisomo.sakala@mombo.solutions>
2016-06-17 00:28:06 -0500

    minor refactor changes

--- config.sh
+++ config.sh
@@ -2,7 +2,8 @@
 #Configs
 
 my_gitdir="$HOME/ec2site.git"
-my_worktree=/opt/bitnami/apache2/htdocs/ec2site.com
+my_worktree="/opt/bitnami/apache2/htdocs/ec2site.com"
 my_origin="ssh://git@bitbucket.org/makombo/test.git"
-#https://makombo@bitbucket.org/makombo/test.git
+my_branch="master"
+
 
