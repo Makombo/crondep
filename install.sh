c18091fc5c506f0dd2efb78c4f2a70ea197ff87a
Somo Makombo <chisomo.sakala@mombo.solutions>
2016-06-17 00:28:06 -0500

    minor refactor changes

--- install.sh
+++ install.sh
@@ -24,37 +24,30 @@ echo "... doing git init at $my_gitdir"
  $(which git) init --bare $my_gitdir
 my_git="$(which git) --work-tree=$my_worktree --git-dir=$my_gitdir" 
 
-#echo "...  backing up worktree $my_worktree to original_backup branch at $my_origin"
 echo "...  checking if worktree $my_worktree already exists"
 if [ -d "$my_worktree" ]; then
-  error_exit " ... Working tree already exists"
-	#$my_git branch original_backup
-	#$my_git checkout original_backup
-	#$my_git add .
-	#$my_git commit -m "Initial commit for backup of original"
-	#$my_git push -u $my_origin original_backup 
-	#$my_git checkout master
+	error_exit " ... Working tree already exists"
 else 
 	mkdir -p "$my_worktree"
 fi
 
 echo "... doing git pull from $my_origin"
-  $my_git pull $my_origin master 
+$my_git pull $my_origin master 
   
 
-cat << EOF > $the_post_receive_hook
+cat > $the_post_receive_hook << EOF
 #!/bin/sh
 $my_git checkout -f
 EOF
 
-cat << EOF > $the_pre_receive_hook
+cat > $the_pre_receive_hook << EOF
 #!/bin/sh
 $my_git clean -fd
 $my_git reset --hard HEAD
 EOF
 
 mkdir -p $my_gitdir/puller
-cat << EOF > $the_puller
+cat > $the_puller << EOF
 #!/bin/sh
 cd $my_gitdir 
 $my_git pull $my_origin master 
@@ -65,7 +58,6 @@ chmod +x $the_post_receive_hook $the_pre_receive_hook $the_puller
 
 echo "... setting cron job"
 (crontab -l ; echo "* * * * * $the_puller > $the_log 2>&1") | sort - | uniq - | crontab -
-#(crontab -l ; echo "0 * * * * rm $the_log 2>&1") | sort - | uniq - | crontab -
 
 echo ""
 echo "===== Installation Complete !! ====="
